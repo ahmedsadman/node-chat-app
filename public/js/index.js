5d8ff03c95347f221a41efd8e16639d89307884c
Ahmed Sadman <ahmedsadman.211@gmail.com>
2019-01-04 15:14:24 +0600

    Add geolocation support

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,10 @@
 const socket = io();
 
+const element = {
+    messages: document.getElementById('messages'),
+    messageInput: document.querySelector('input[name="message"]'),
+};
+
 socket.on('connect', () => {
     console.log('Connected to server');
 });
@@ -11,16 +16,36 @@ socket.on('disconnect', () => {
 socket.on('newMessage', (message) => {
     console.log('newMessage: ', message);
     const html = `<li>${message.from}: ${message.text}</li>`;
-    document.getElementById('messages').insertAdjacentHTML('beforeend', html);
+    element.messages.insertAdjacentHTML('beforeend', html);
+});
+
+socket.on('newLocationMessage', (message) => {
+    const html = `<li>${message.from}: <a href="${message.url}" target="_blank">My Current Location</a></li>`;
+    element.messages.insertAdjacentHTML('beforeend', html);
 });
 
 document.querySelector('#message-form').onsubmit = (e) => {
     e.preventDefault();
     socket.emit('createMessage', {
         from: 'User',
-        text: document.querySelector('input[name="message"]').value,
+        text: element.messageInput.value,
     }, () => {
         console.log('Got it!');
     });
-    document.querySelector('input[name="message"').value = ''; // clear input
+    element.messageInput.value = ''; // clear input
+};
+
+const locationButton = document.getElementById('send-location');
+
+locationButton.onclick = (e) => {
+    if (!navigator.geolocation) return alert('Geolocation not supported by your browser');
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+        });
+    }, (error) => {
+        alert('Unable to fetch location: ', error);
+    });
 };
--- server/utils/message.js
+++ server/utils/message.js
@@ -6,6 +6,15 @@ const generateMessage = (from, text) => {
     };
 };
 
+const generateLocationMessage = (from, latitude, longitude) => {
+    return {
+        from,
+        url: `https://www.google.com/maps?q=${latitude},${longitude}`,
+        createdAt: new Date().getTime(),
+    };
+};
+
 module.exports = {
     generateMessage,
+    generateLocationMessage,
 };
